ef4de2764e4a0ca95727164abc59d125f0da8844
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2023-09-29 20:41:37 +0100

    Atualizar o app.py

--- app.py
+++ app.py
@@ -140,7 +140,8 @@ fig_product_sales = px.bar(
     width=800,
     height=1200
 )
-
+fig.update_layout(
+    dragmode='pan'
 #fig_product_sales.update_traces(marker=dict(line=dict(width=2, color='DarkSlateGrey')))  # Adicione uma borda às barras
 fig_product_sales.update_layout(plot_bgcolor="rgba(0,0,0,0)")
 fig_product_sales.update_layout(yaxis_title="Marca", xaxis_title="Valor Líquido")
@@ -167,6 +168,8 @@ fig_product_client = px.bar(
     height= altura_desejada
    
 )
+fig.update_layout(
+    dragmode='pan'
 #fig_product_client.update_traces(marker=dict(line=dict(width=2, color='DarkSlateGrey')))  # Adicione uma borda às barras
 fig_product_client.update_layout(plot_bgcolor="rgba(0,0,0,0)")
 fig_product_client.update_coloraxes(showscale=False)
