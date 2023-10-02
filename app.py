2c670d50454906786f4fed45cdb8f07208643f5d
Tiago Mazza <tiagomazza@gmail.com>
2023-10-02 16:15:17 +0100

    pad solucao

--- app.py
+++ app.py
@@ -142,10 +142,7 @@ fig_product_sales = px.bar(
 )
 
 #fig_product_sales.update_traces(marker=dict(line=dict(width=2, color='DarkSlateGrey')))  # Adicione uma borda às barras
-fig_product_sales.update_layout(
-    xaxis=dict(rangemode="pad"),
-    yaxis=dict(rangemode="pad"),
-)
+
 fig_product_sales.update_layout(plot_bgcolor="rgba(0,0,0,0)")
 fig_product_sales.update_layout(yaxis_title="Marca", xaxis_title="Valor Líquido")
 fig_product_sales.update_coloraxes(showscale=False)
@@ -173,10 +170,7 @@ fig_product_client = px.bar(
 )
 
 #fig_product_client.update_traces(marker=dict(line=dict(width=2, color='DarkSlateGrey')))  # Adicione uma borda às barras
-fig_product_client.update_layout(
-    xaxis=dict(rangemode="pad"),
-    yaxis=dict(rangemode="pad"),
-)
+
 fig_product_client.update_layout(plot_bgcolor="rgba(0,0,0,0)")
 fig_product_client.update_coloraxes(showscale=False)
 st.plotly_chart(fig_product_client)
